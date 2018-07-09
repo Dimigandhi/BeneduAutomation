56b8389c902d4d44c59e0ead82eb08c43b1183f8
1kl1 <mamy0320@naver.com>
2018-07-09 14:20:13 +0900

    Get Answer to DB, v1

--- crawl.py
+++ crawl.py
@@ -7,7 +7,7 @@ import random
 
 sql_pw = input("sql 비밀번호를 입력하세요:")
 
-conn = pymysql.connect(host='localhost', port=3306, user='1kl1', password=sql_pw, database='benedu')
+conn = pymysql.connect(host='localhost', port=3306, user='benedu', password='benedudbpass!', database='benedu')
 cursor = conn.cursor()
 cursor.execute('SELECT * FROM answersheet;')
 rows = cursor.fetchall()
@@ -30,15 +30,19 @@ def gotoPage(driver):
     driver.find_element_by_css_selector('li#mnu03StdStudy.dropdown').click()
     driver.find_element_by_css_selector('a[href="03StdStudy02PaperTestList.aspx"]').click()
     driver.implicitly_wait(3)
+    #
     driver.find_element_by_xpath('//*[text()[contains(.,\'ㅂㅇㄹㅂㅇㄹ\')]]').click()
     itsnumber = str(driver.find_element_by_xpath('//*[@id="DT_TestList"]/tbody/tr[1]/td[2]').get_attribute("onclick"))
     itsnumber = itsnumber[itsnumber.find("ShowPop(\"")+9:itsnumber.find("\", ")]
     value = 1
+    # 밑에 value <= [문제지 개수임 ㅋㅋ]
     while(value<=1):
         driver.execute_script("DoTakeExam("+itsnumber+","+str(value)+")")
         driver.implicitly_wait(2)
         solve(driver)
         value += 1
+        sqlflag = [0,0,0,0,0]
+
     
 # 문항번호가 몇자리인지 몰라서만든 함수.
 def toInt(tmpString):
@@ -66,8 +70,21 @@ def checking(driver, probIndex, answerNum):
     js_script = "ClickAnswer(\""+str(probIndex)+"\",\""+str(answerNum)+"\")"
     driver.execute_script(js_script)
 
-def getAnswerToDB(driver):
-    print("test")
+def getAnswerToDB(driver,probNum):
+    for i in range(5):
+        j = 0
+        for j in range(5):
+            if(sqlflag[i]==0):
+                break
+            select = driver.find_element_by_xpath("//*[@id=\"frmBenedu\"]/div[3]/section[2]/div[2]/div[3]/div/div/div[1]/div/div/table/tbody/tr["+str(i+1)+"]/td["+str(j+2)+"]/span")
+            if(select.get_attribute("class")=="badge bg_red"):
+                answerReal = select.text()
+                sql = "INSERT INTO answersheet (answer,author,created,category,number) VALUES("+answerReal+",,NOW(),'여기',"+probNum[i]+");"
+                cursor.execute('SELECT * FROM answersheet;')
+            
+
+        
+    
     
 
 
@@ -101,9 +118,12 @@ def solve(driver):
         answer = checkProbNum(tmpval, probnum[tmpval])
         checking(driver, tmpval, answer)
         tmpval+=1
+    
+    # 시작시간 해결이 안되면 이 부분에 딜레이가 있어야 함.
+    driver.execute_script("grecaptcha = undefined") #캡챠 무시
     driver.find_element_by_xpath("//*[@id=\"btnSubmit\"]").click()
 
-    getAnswerToDB(driver)
+    getAnswerToDB(driver,probnum) # 채점을 받고 받은 데이터를 DB에 저장하는 과정.
 
     
 # 이부분이 메인
@@ -116,5 +136,3 @@ except:
     gotoPage(driver)
     conn.close()
     #gotoPage 함수에선 문제를 생성은 안하고 푸는것만 함. 아직 미완성 안에 solve함수를 문제 시트마다 접근한다.
-
-
--- for_test.py
+++ for_test.py
@@ -1,6 +1,2 @@
-import pymysql
-
-
-sqlflag = [0,2,0,0,0]
-
-print(sqlflag[1])
+for i in range(5):
+    print("1")
